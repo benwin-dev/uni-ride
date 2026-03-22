fdfb03d71e4326b61b8571f078758eb4bcaf0ff1
Benwin <benwingeorge@gmail.com>
2026-03-22 00:11:26 -0400

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Outfit } from "next/font/google";
 import { Providers } from "@/components/Providers";
 import "./globals.css";
 
+import Script from "next/script";
+
 const outfit = Outfit({
   variable: "--font-outfit",
   subsets: ["latin"],
@@ -23,6 +25,18 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${outfit.variable} font-sans antialiased`}>
         <Providers>{children}</Providers>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-91CJYDXVG8');
+          `}
+        </Script>
       </body>
     </html>
   );
